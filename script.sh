f651b4c4ea632c016d13a0579e258881910f2929
gb8462 <planetes8462@gmail.com>
2025-04-16 07:44:27 +0800

    update

--- script.sh
+++ script.sh
@@ -7,9 +7,14 @@ echo "Please Select which Themes you would like to use:"
 echo "| [1]Chill | [2]Ninomae Ina'nis |"
 read -p "Theme: " Themes
 if [ "$Themes" == "1" ]; then
-  echo "test 1"
+  cp -r /home/worm/Programming/WallTheme/Blues/waybar/* ~/.config/waybar
+  cp -r /home/worm/Programming/WallTheme/Blues/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+  sudo cp -r /home/worm/Programming/WallTheme/Blues/slick-greeter.conf /etc/lightdm/slick-greeter.conf
 elif [ "$Themes" == '2' ]; then
-  echo "test 2"
+  cp -r /home/worm/Programming/WallTheme/Ina/waybar/* ~/.config/waybar
+  cp -r /home/worm/Programming/WallTheme/Ina/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+  sudo cp -r /home/worm/Programming/WallTheme/Ina/slick-greeter.conf /etc/lightdm/slick-greeter.conf
 else
   echo "invalid input"
 fi
+
